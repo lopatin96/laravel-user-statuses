04b924c4d0744dacc73725c5b246af3707b7c8c0
vladyslavlopatin <ilopatin3@gmail.com>
2023-06-15 19:52:56 +0200

    cancel subscription on blocked

--- src/Http/Middleware/EnsureUserIsNotBlocked.php
+++ src/Http/Middleware/EnsureUserIsNotBlocked.php
@@ -4,13 +4,16 @@ namespace Atin\LaravelUserStatuses\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class EnsureUserIsNotBlocked
 {
     public function handle(Request $request, Closure $next): mixed
     {
-        if (Auth::user()?->isBlocked()) {
+        if (auth()->user()?->isBlocked()) {
+            if (optional(auth()->user()->subscription())->recurring()) {
+                auth()->user()->subscription()->cancel();
+            }
+
             session()->flush();
             abort(403, __('laravel-user-statuses::user-statuses.Your account is blocked'));
         }
