48ed6db2fec4a836f0562591ec426c610696377f
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-10-04 15:47:33 +0200

    added scopes

--- src/Traits/HasUserStatus.php
+++ src/Traits/HasUserStatus.php
@@ -2,12 +2,29 @@
 
 namespace Atin\LaravelUserStatuses\Traits;
 
+use App\Models\User;
 use Atin\LaravelUserStatuses\Enums\UserStatus;
+use Illuminate\Database\Eloquent\Builder;
 
 trait HasUserStatus
 {
     public function isBlocked(): bool
     {
         return $this->status === UserStatus::Blocked;
+    }
+
+    public function scopeActive(Builder $query): void
+    {
+        $query->where('status', '=', UserStatus::Active);
+    }
+
+    public function scopeRestricted(Builder $query): void
+    {
+        $query->where('status', '=', UserStatus::Restricted);
+    }
+
+    public function scopeBlocked(Builder $query): void
+    {
+        $query->where('status', '=', UserStatus::Blocked);
     }
 }
